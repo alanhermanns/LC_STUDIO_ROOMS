39ef179f09b27b4b45ac5a818295359e6a6e84ae
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-08-27 13:24:44 -0700

    !

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,22 +6,23 @@ import { useSocketState, useEmitEvent } from 'react-socket-io-hooks';
 
 const Calendar = (props) => {
   //   const user = useUser();
-  const emit = useEmitEvent('NEW_TIME');
-
+  const emitNewTime = useEmitEvent('NEW_TIME');
+  const emitRetriveTimes = useEmitEvent('RETRIEVE_TIMES');
   const [room, setRoom] = useState('')  
   const state = useSocketState();
   console.log(state);
 
   useEffect(() => {
+    emitRetriveTimes({payload : ''})
     setRoom(localStorage.getItem('room name'));
   }, []);
-
-const times = () => {
+  
+  const times = () => {
     let arrOfTimes = [6,8,10,12,2,4,6,8,10]
     console.log(Date.UTC);
     const timeElements = arrOfTimes.map((time) => {
         return (
-        <button className={ButtonStyles.button3} value={time} onClick={() => emit({
+        <button className={ButtonStyles.button3} value={time} onClick={() => emitNewTime({
           payload : {
           time: event.target.value,
           roomName: room
