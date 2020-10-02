d6ab14863335774840fed4b5de8c051ff614b546
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-10-01 20:26:26 -0700

    app

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import Login2 from './Login2';
 
 export default function App() {
   return (
-    <SocketProvider uri="localhost:7890"
+    <SocketProvider uri="https://lc-rooms-be.herokuapp.com/"
     reducer={reducer}
     initialState={{
       takenTimes: [{
